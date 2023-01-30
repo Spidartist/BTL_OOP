ffcd3a193eee8a477b98210eb91591ad58d26c2c
hoang.danh.quan <hoang.danh.quan@sun-asterisk.com>
2023-01-30 23:03:47 +0700

    smt

--- src/application/ReadJson.java
+++ src/application/ReadJson.java
@@ -19,7 +19,7 @@ public class ReadJson {
     private ObservableList<Figure> figureList = FXCollections.observableArrayList();
 
     public ObservableList<King> getKingList() {
-        JSONArray dataList = readData("src/jSoupWebCrawler/jsonFiles/king.json");
+        JSONArray dataList = readData("src/webcrawler/jsonFiles/king.json");
         for (int i = 0; i < dataList.size(); i++) {
             kingList.add(new King().parseDataObject((JSONObject) dataList.get(i)));
         }
@@ -27,7 +27,7 @@ public class ReadJson {
     }
 
     public ObservableList<Figure> getFigureList() {
-        JSONArray dataList = readData("src/jSoupWebCrawler/jsonFiles/figure.json");
+        JSONArray dataList = readData("src/webcrawler/jsonFiles/figure.json");
         for (int i = 0; i < dataList.size(); i++) {
             figureList.add(new Figure().parseDataObject((JSONObject) dataList.get(i)));
         }
