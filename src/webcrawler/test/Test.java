4ccd01c799f754b29bdf0f69e36a1ea27eb2df38
vandanghuy9 <vandanghuy9@gmail.com>
2023-01-31 22:19:58 +0700

    update figure

---
+++ src/webcrawler/test/Test.java
@@ -0,0 +1,26 @@
+package webcrawler.test;
+import webcrawler.parent.BasicWebScraper;
+import webcrawler.parent.IScraping;
+
+import java.util.ArrayList;
+
+import objects.dynasty.Dynasty;
+import objects.figure.Figure;
+import objects.figure.King;
+public class Test {
+	public static void main(String[] args) {
+		Dynasty d1 = new Dynasty("Thời tiền sử");
+		ArrayList<Dynasty> list = new ArrayList<Dynasty>();
+		Dynasty d2 = new Dynasty("đầu","3100 TCN","Thời tiền sử");
+		Dynasty d4 = new Dynasty("204 TCN","111 TCN","Nhà Triệu");
+		list.add(d4);
+		list.add(d2);
+		
+		for (Dynasty d : list) {
+			if (d.getName().contains(d1.getName())) {
+				d1 = d;
+			}
+		}
+	}
+	
+}
