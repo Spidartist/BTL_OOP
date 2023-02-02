77958f3c14d440beb5275e6a3f38c77be8e7eb32
Spidartist <72654978+Spidartist@users.noreply.github.com>
2023-02-02 15:55:48 +0700

    Update Dynasty.java

--- src/objects/dynasty/Dynasty.java
+++ src/objects/dynasty/Dynasty.java
@@ -1,28 +1,19 @@
 package objects.dynasty;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
+
+import objects.figure.King;
 
 public class Dynasty {
 	private String startYear; // co ve de
 	private String endYear; // chua code
 	private String name; // xong, day la base
-	private ArrayList<String> kings; // 1 phan, 8 ban ghi
+	private LinkedList<King> kings; // 1 phan, 8 ban ghi
 	private String capital; // co ve day la thu do thi hop ly hon
-	// private ArrayList<String> events;
-	private String founder; // done
+	private King founder; // done
 
 	public Dynasty() {
 
-	}
-
-	public Dynasty(String startYear, String endYear, String name, ArrayList<String> kings, String capital,
-			String founder) {
-		this.startYear = startYear;
-		this.endYear = endYear;
-		this.name = name;
-		this.kings = kings;
-		this.capital = capital;
-		this.founder = founder;
 	}
 
 	public Dynasty(String name) {
@@ -30,12 +21,15 @@ public class Dynasty {
 		this.name = name;
 	}
 
-	public Dynasty(String startYear, String endYear, String name, ArrayList<String> kings) {
+	public Dynasty(String startYear, String endYear, String name, LinkedList<King> kings, String capital,
+			King founder) {
 		super();
 		this.startYear = startYear;
 		this.endYear = endYear;
 		this.name = name;
 		this.kings = kings;
+		this.capital = capital;
+		this.founder = founder;
 	}
 
 	public Dynasty(String startYear, String endYear, String name) {
@@ -45,20 +39,11 @@ public class Dynasty {
 		this.name = name;
 	}
 
-	public Dynasty(String startYear, String endYear, String name, ArrayList<String> kings, String location) {
-		super();
-		this.startYear = startYear;
-		this.endYear = endYear;
-		this.name = name;
-		this.kings = kings;
-		this.capital = location;
-	}
-
-	public String getFounder() {
+	public King getFounder() {
 		return founder;
 	}
 
-	public void setFounder(String founder) {
+	public void setFounder(King founder) {
 		this.founder = founder;
 	}
 
@@ -86,11 +71,11 @@ public class Dynasty {
 		this.name = name;
 	}
 
-	public ArrayList<String> getKings() {
+	public LinkedList<King> getKings() {
 		return kings;
 	}
 
-	public void setKings(ArrayList<String> kings) {
+	public void setKings(LinkedList<King> kings) {
 		this.kings = kings;
 	}
 
