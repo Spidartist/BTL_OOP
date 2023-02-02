f15766b78b2bc1f2ebbec83f9a39d4be0d68f6dc
jenybo <86452879+JenyBo@users.noreply.github.com>
2023-02-02 16:44:08 +0700

    Search_update

---
+++ src/application/search/Search.java
@@ -0,0 +1,12 @@
+package application.search;
+
+import javafx.collections.transformation.FilteredList;
+import objects.figure.King;
+
+public class Search {
+//	Run for King
+//	FilteredList<King> filteredDataKing = new FilteredList<>(listObservablesKing);
+//	textField.textProperty().addListener((observable, oldValue, newValue) -> {
+//		filteredDataKing.setPredicate(createPredicateKing(newValue))
+//    }
+}
---
+++ src/application/search/details/SearchDetails.java
@@ -0,0 +1,5 @@
+package application.search.details;
+
+public abstract class SearchDetails {
+
+}
