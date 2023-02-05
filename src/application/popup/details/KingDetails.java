b88d8340da7ae053768313adf4f09fb3d48a7f57
jenybo <86452879+JenyBo@users.noreply.github.com>
2023-02-05 16:57:58 +0700

    update_popup_css

--- src/application/popup/details/KingDetails.java
+++ src/application/popup/details/KingDetails.java
@@ -6,9 +6,16 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import objects.figure.King;
 
@@ -17,12 +24,15 @@ public class KingDetails {
         BorderPane borderPane = new BorderPane();
         Stage stage = new Stage();
         stage.setTitle("Figure Detail");
-        Image image = new Image(
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSufu-xJiSOxynzT2dbbwlAGaP5Gm-TnGM2IA&usqp=CAU");
+        Image imagebackground = new Image("https://media.discordapp.net/attachments/755083836169257062/1071699179488944128/image.png?width=1190&height=670");
+        BackgroundImage backgroundImage = new BackgroundImage(imagebackground, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+        borderPane.setBackground(new Background(backgroundImage));
+        
+        Image image = new Image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSufu-xJiSOxynzT2dbbwlAGaP5Gm-TnGM2IA&usqp=CAU");
         ImageView imageView = new ImageView(image);
-        borderPane.setLeft(imageView);
-
-        BorderPane.setAlignment(imageView, Pos.CENTER);
+//        imageView.setStyle("-fx-border-color: red; -fx-border-width: medium;-fx-border-style: solid;");
+        imageView.getStyleClass().add("border-style");
+        
         Label ten = new Label("Tên Vua: " + curSelect.getTen());
         Label namTriVi = new Label("Năm trị vì: " + curSelect.getNamTriVi());
         Label theThu = new Label("Tên thứ: " + curSelect.getTheThu());
@@ -31,19 +41,43 @@ public class KingDetails {
         Label thuyHieu = new Label("Thụy hiệu: " + curSelect.getThuyHieu());
         Label mieuHieu = new Label("Miếu hiệu: " + curSelect.getMieuHieu());
         Label paperURL = new Label("Link bài báo tìm hiểu thêm: " + curSelect.getPaperURL());
-        VBox contentText = new VBox(10);
-        contentText.setPadding(new Insets(20, 20, 20, 20));
+        
+        ten.getStyleClass().add("text-color");
+        namTriVi.getStyleClass().add("text-color");
+        theThu.getStyleClass().add("text-color");
+        tenHuy.getStyleClass().add("text-color");
+        nienHieu.getStyleClass().add("text-color");
+        thuyHieu.getStyleClass().add("text-color");
+        mieuHieu.getStyleClass().add("text-color");
+        paperURL.getStyleClass().add("text-color");
+        
+        VBox vbox = new VBox();
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(20,20,20,20));
+        hbox.setSpacing(25);
+        HBox picturebox = new HBox();
+        //picturebox.setSpacing(20);
+        picturebox.getChildren().add(imageView);
+        picturebox.setStyle("-fx-border-color: white; -fx-border-width: 3px; -fx-effect : dropshadow(one-pass-box,white, 5, 5, 0, 0);");
+        
+        VBox labelbox = new VBox();
+        //labelbox.setSpacing(20);
+        labelbox.getChildren().addAll(ten,namTriVi,theThu,tenHuy,nienHieu,thuyHieu,mieuHieu);
+        
+        hbox.getChildren().addAll(picturebox,labelbox);
+        
+        VBox contentText = new VBox();
+        contentText.getChildren().addAll(paperURL);
+        
+        vbox.getChildren().addAll(hbox,contentText);
+        borderPane.setCenter(vbox);
+        picturebox.setAlignment(Pos.CENTER_LEFT);
+        labelbox.setAlignment(Pos.BASELINE_LEFT);
+        hbox.setAlignment(Pos.CENTER);
         contentText.setAlignment(Pos.CENTER);
-        contentText.getChildren().add(ten);
-        contentText.getChildren().add(namTriVi);
-        contentText.getChildren().add(theThu);
-        contentText.getChildren().add(tenHuy);
-        contentText.getChildren().add(nienHieu);
-        contentText.getChildren().add(thuyHieu);
-        contentText.getChildren().add(mieuHieu);
-        contentText.getChildren().add(paperURL);
-        borderPane.setCenter(contentText);
+        vbox.setAlignment(Pos.CENTER);
         Scene scene = new Scene(borderPane, 800, 600);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
--- src/application/popup/details/RelicDetails.java
+++ src/application/popup/details/RelicDetails.java
@@ -12,6 +12,11 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -26,15 +31,17 @@ public class RelicDetails {
             ObservableList<Dynasty> listDynasty) {
         BorderPane borderPane = new BorderPane();
         Stage stage = new Stage();
-        stage.setTitle("Figure Detail");
+        stage.setTitle("Relic Detail");
+        
+        Image imagebackground = new Image("https://media.discordapp.net/attachments/755083836169257062/1071699179488944128/image.png?width=1190&height=670");
+        BackgroundImage backgroundImage = new BackgroundImage(imagebackground, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+        borderPane.setBackground(new Background(backgroundImage));
+        
         Image image = new Image(
                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSufu-xJiSOxynzT2dbbwlAGaP5Gm-TnGM2IA&usqp=CAU");
         ImageView imageView = new ImageView(image);
-        borderPane.setLeft(imageView);
-
-        BorderPane.setAlignment(imageView, Pos.CENTER);
-
-        Label name = new Label("Tên di tích " + curSelect.getName());
+     
+        Label name = new Label("Tên di tích: " + curSelect.getName());
         Label location = new Label("Địa điểm: " + curSelect.getLocation());
         Label type = new Label("Kiểu di tích: " + curSelect.getType());
         Label rank = new Label("Di tích cấp: " + curSelect.getRank());
@@ -90,29 +97,42 @@ public class RelicDetails {
         Label vua = new Label("Vua: " + (strKings == "" ? "Không có" : strKings));
         Label nhanVat = new Label(
                 "Nhân vật Lịch sử: " + (strFigures == "" ? "Không có" : strFigures));
-        VBox contentText = new VBox(10);
-        contentText.setPadding(new Insets(20, 20, 20, 20));
+        
+        name.getStyleClass().add("text-color");
+        location.getStyleClass().add("text-color");
+        type.getStyleClass().add("text-color");
+        rank.getStyleClass().add("text-color");
+        desc.getStyleClass().add("text-color");
+        trieuDai.getStyleClass().add("text-color");
+        vua.getStyleClass().add("text-color");
+        nhanVat.getStyleClass().add("text-color");
+        
+        VBox vbox = new VBox();
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(20,20,20,20));
+        hbox.setSpacing(25);
+        HBox picturebox = new HBox();
+        //picturebox.setSpacing(20);
+        picturebox.getChildren().add(imageView);
+        picturebox.setStyle("-fx-border-color: white; -fx-border-width: 3px; -fx-effect : dropshadow(one-pass-box,white, 5, 5, 0, 0);");
+        
+        VBox labelbox = new VBox();
+        //labelbox.setSpacing(20);
+        labelbox.getChildren().addAll(name,location,type,rank,trieuDai,vua,nhanVat);
+        
+        hbox.getChildren().addAll(picturebox,labelbox);
+        
+        VBox contentText = new VBox();
+        contentText.getChildren().addAll(desc);
+        contentText.setPadding(new Insets(10, 50, 10, 50));
+        
+        vbox.getChildren().addAll(hbox,contentText);
+        borderPane.setCenter(vbox);
+        picturebox.setAlignment(Pos.CENTER_LEFT);
+        labelbox.setAlignment(Pos.BASELINE_LEFT);
+        hbox.setAlignment(Pos.CENTER);
         contentText.setAlignment(Pos.CENTER);
-        contentText.getChildren().add(name);
-        contentText.getChildren().add(location);
-        contentText.getChildren().add(type);
-        contentText.getChildren().add(rank);
-        contentText.getChildren().add(desc);
-        contentText.getChildren().add(trieuDai);
-        contentText.getChildren().add(vua);
-        contentText.getChildren().add(nhanVat);
-        borderPane.setCenter(contentText);
-
-        // BorderPane.setAlignment(contentText, Pos.CENTER);
-
-        // moreInfoButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent e) -> {
-        // for (int i = 0; i < curSelect.getTrieuDai().size(); i++) {
-        // new DynastyDetails(curSelect.getTrieuDai().get(i));
-        // System.out.println("1");
-        // }
-        // });
-        // borderPane.setBottom(moreInfoButton);
-        // BorderPane.setAlignment(moreInfoButton, Pos.CENTER);
+        vbox.setAlignment(Pos.CENTER);
         HBox moreInforContainer = new HBox();
 
         for (int i = 0; i < curSelect.getDynastys().size(); i++) {
@@ -142,9 +162,8 @@ public class RelicDetails {
             moreInforContainer.getChildren().add(moreInfoButton);
         }
 
-        borderPane.setBottom(moreInforContainer);
-        BorderPane.setAlignment(moreInforContainer, Pos.CENTER);
         Scene scene = new Scene(borderPane, 800, 600);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
